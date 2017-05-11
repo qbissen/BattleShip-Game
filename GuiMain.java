01b3635f1e00eb852a40215e670a399b70bbf49f
QuantonBiscuit <QuantonBiscuit@users.noreply.github.com>
2017-05-10 20:22:58 -0400

    Add files via upload

--- GuiMain.java
+++ GuiMain.java
@@ -3,6 +3,11 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  * Created by Quinn on 2/21/2017.
  * guiMain creates and adds listeners for the GUI, it also sets up the actions that the user can input into the game.
@@ -36,6 +41,14 @@ public class GuiMain extends JFrame{
     private static String IP_ADDR;
     private JButton button;
     private JButton button1;
+
+    //Chat client variables
+    final int PORT = 16789;
+    private JTextArea jtaMessages;
+    private JTextField jtfSendMessage;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oout;
+    private Socket socket;
 
     public static void main(String []args){
      
@@ -442,4 +455,92 @@ public class GuiMain extends JFrame{
             checkWin(logicClass.getLogicBottomBoard(), "Top");
         }
     };
+
+    public class ChatClient extends JPanel implements Runnable{
+        public ChatClient(String _IPADDR) {
+            try{
+                socket = new Socket(_IPADDR, 16789);
+            }catch(IOException ioe){
+
+            }
+            createStreams();
+            System.out.println("Created the ChatClientGUI constructor");
+            setLayout(new BorderLayout());
+            JLabel jlTitle =
+                    new JLabel("In Game Chat Client",JLabel.CENTER );
+
+            add( jlTitle, BorderLayout.NORTH );
+
+            jtaMessages = new JTextArea(20,30);
+            jtaMessages.setLineWrap(true);
+            jtaMessages.setWrapStyleWord(true);
+            jtaMessages.setEditable(false);
+
+            JScrollPane jspText = new JScrollPane(jtaMessages);
+
+            add(jspText, BorderLayout.CENTER );
+
+            // Add a field for message entry and a SEND button to send it
+            JPanel jpSendingInfo = new JPanel();
+            jtfSendMessage = new JTextField(25);
+            jpSendingInfo.add(jtfSendMessage);
+            JButton jpSend = new JButton("Send");
+
+            jpSend.addActionListener(new ActionListener() {
+                /**
+                 * Responds to pressing the enter key in the textfield by sending
+                 * the contents of the text field to the server.    Then clear
+                 * the text area in preparation for the next message.
+                 */
+                public void actionPerformed(ActionEvent e) {
+                    sendMessage();
+                }
+            });
+            jpSendingInfo.add(jpSend);
+            add( jpSendingInfo, BorderLayout.SOUTH);
+            setVisible(true);
+            jtfSendMessage.requestFocus();
+            System.out.println("Just before createStreams");
+
+
+            Thread cc = new Thread(this);//readMessage();
+            cc.start();
+        }
+        public void createStreams(){
+            try {
+                // open input stream
+                oout = new ObjectOutputStream(socket.getOutputStream());
+                ois = new ObjectInputStream(socket.getInputStream());
+
+                // open output stream
+
+                System.out.println("Created input and output streams");
+            } catch(IOException ioe){
+
+            }
+        }
+
+        public void sendMessage(){
+            try{
+                oout.writeUTF("CHAT");
+                oout.writeUTF(jtfSendMessage.getText());
+                oout.flush();
+            }catch(IOException ioe){
+
+            }
+            jtfSendMessage.setText("");
+        }
+        public void run(){
+            String mes = "";
+            try {
+                while ((mes = ois.readUTF())!=null ) {
+
+                    System.out.println(mes);
+                    jtaMessages.append( ": " + mes + " \n");
+                }
+            }catch (IOException ioe) {
+
+            }
+        }
+    }
 }
